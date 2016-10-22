fd794458499bc1d1d4af8c3f89a8c73a17e720af
Muneer Hudha <muneerulhudha@gmail.com>
2016-10-22 13:46:36 -0500

    Hello World done

---
+++ backend/src/ubercheap/test/HelloService.java
@@ -0,0 +1,16 @@
+package ubercheap.test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("hello")
+public class HelloService {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello() {
+		return "hello";
+	}
+}
