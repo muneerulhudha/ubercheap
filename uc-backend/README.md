9070288fce8344e5ac1e46ba1329b5cae575159d
Muneer Hudha <muneerulhudha@gmail.com>
2016-10-22 15:32:05 -0500

    Removed Java project and using Sails

---
+++ uc-backend/README.md
@@ -0,0 +1,3 @@
+# uc-backend
+
+a [Sails](http://sailsjs.org) application
---
+++ uc-backend/api/controllers/TestController.js
@@ -0,0 +1,16 @@
+/**
+ * TestController
+ *
+ * @description :: Server-side logic for managing tests
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+	hello: function(req, res){
+		
+		return res.send("hello", 200);
+	}
+
+};
+
